bf3c085afddb712a332773dcc37714496512a2bd
hukurouo <owlbook248@gmail.com>
2020-12-01 00:01:58 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container min-h-screen justify-center items-center text-center mx-auto p-20">
+    <User :user="user_obj"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+export default defineComponent({
+  setup () {
+    const name = ref('This is a name')
+    const user_obj = ref({firstName: "ai", 
+                          lastName:  "ueo"})
+    return {
+      name,
+      user_obj
+    }
+  }
+})
+</script>
