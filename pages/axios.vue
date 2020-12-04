76f279632bbaf9b42bcfdfc754e597328c410239
hukurouo <owlbook248@gmail.com>
2020-12-05 00:03:53 +0900

    loading

--- pages/axios.vue
+++ pages/axios.vue
@@ -2,7 +2,7 @@
   <div class="justify-center items-center text-center mx-auto">
     <h1>randomCat (axios)</h1>
     <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" @click="changeCat">
-      change 🐱
+      {{ state.loading }} 🐱
     </button>
     <br><br>
     <img :src="state.cat_url" alt="">
@@ -21,11 +21,14 @@ import { $axios } from '~/utils/api'
 export default defineComponent({
   setup () {
     const state = reactive({
-      cat_url: '' as string
+      cat_url: '' as string,
+      loading: 'loading...' as string
     })
     const asyncFunc = async (): Promise<void> => {
+      state.loading = 'loading...'
       const CatObj: {file: string} = await $axios.$get('https://aws.random.cat/meow')
       state.cat_url = CatObj.file
+      state.loading = 'change !'
     }
     const changeCat = () => { asyncFunc() }
 
