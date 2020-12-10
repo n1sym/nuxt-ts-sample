cee9113158904c6d64a139fe019c1b0109a54696
hukurouo <owlbook248@gmail.com>
2020-12-11 01:20:46 +0900

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ const nuxtConfig: NuxtConfig = {
   css: [
   ],
   env: {
-    API_KEY: process.env.API_KEY
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGE_SENDER_ID: process.env.MESSAGE_SENDER_ID,
+    MEASUREMENR_ID: process.env.MEASUREMENR_ID
   },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
