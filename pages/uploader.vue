bbb2f9a46828ffcd31152c6d08c3904fa6a83132
hukurouo <owlbook248@gmail.com>
2020-12-12 00:18:06 +0900

    認証２

--- pages/uploader.vue
+++ pages/uploader.vue
@@ -11,7 +11,11 @@
         SignOut
       </button>
       <br><br>
-      {{ state.displayName }} でログインしています {{ state.authStatus }}
+      {{ state.displayName }} でログインしています
+      <br><br>
+      <div v-if="state.authStatus">
+        ✅ 認証済み
+      </div>
     </div>
   </div>
 </template>
@@ -38,39 +42,39 @@ export default defineComponent({
       const user = credential.user!
       state.displayName = user.displayName
       state.loginStatus = true
+      isAuth(user.email)
     }
 
     const signOut = () => {
       firebase.auth().signOut().then(function () {
-        // Sign-out successful.
         state.displayName = ''
         state.loginStatus = false
       }).catch(function (error) {
-        // eslint-disable-next-line no-console
         console.log(error)
-        // An error happened.
       })
     }
 
-    onMounted(async (): Promise<void> => {
-      await firebase.auth().onAuthStateChanged(async (user) => {
-        let email: string | undefined = ''
-        await docRef.get().then(function (doc) {
-          if (doc.exists) {
-            email = doc.data()!.email
-          } else {
-          // doc.data() will be undefined in this case
-            console.log('No such document!')
+    const isAuth = (email: string | null) => {
+      docRef.get().then(function (doc) {
+        if (doc.exists) {
+          if (email === doc.data()!.email) {
+            state.authStatus = true
           }
-        }).catch(function (error) {
-          console.log('Error getting document:', error)
-        })
+        } else {
+        // doc.data() will be undefined in this case
+          console.log('No such document!')
+        }
+      }).catch(function (error) {
+        console.log('Error getting document:', error)
+      })
+    }
+
+    onMounted(() => {
+      firebase.auth().onAuthStateChanged((user) => {
         if (user) {
           state.displayName = user.displayName
           state.loginStatus = true
-          if (email === user.email) {
-            state.authStatus = true
-          }
+          isAuth(user.email)
         } else {
           state.displayName = ''
           state.loginStatus = false
